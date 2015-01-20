12eec1be4291b99ddf54a964709ffe71f8b07866
Fiona-Mary <fiona1harrison@gmail.com>
2015-01-20 11:34:52 +0000

    Create index.php
    Added index as it stands thus far.

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<?php include 'header.php';?>
+<?php include 'nav_bar.php';?>
+  <article id ="PageContent">
+    <h1>Section Title</h1>
+    <section>
+      <p>
+        Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content. Content.
+      </p>
+    </section>
+    <section>
+      <ul>
+        <li>Content. Content. Content. </li>
+        <li>Content. Content. Content. </li>
+        <li>Content. Content. Content. </li>
+        <li>Content. Content. Content. </li>
+        <li>Content. Content. Content. </li>
+        <li>Content. Content. Content. </li>
+        <li>Content. Content. Content. </li>
+        <li>Content. Content. Content. </li>
+        <li>Content. Content. Content. </li>
+        <li>Content. Content. Content. </li>
+        <li>Content. Content. Content. </li>
+        <li>Content. Content. Content. </li>
+        <li>Content. Content. Content. </li>
+        <li>Content. Content. Content. </li>
+      </ul>
+    </section>
+  </article>
+  <footer>
+
+  </footer>
+</body>
+</html>
