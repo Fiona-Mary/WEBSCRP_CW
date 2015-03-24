8a560994f2ab42422cbe87f452735c6e41b0a712
Fiona-Mary <fiona1harrison@gmail.com>
2015-03-24 13:23:20 +0000

    Update and rename nav_bar.php to api/nav_bar.php
    improved code, moved to api folder

---
+++ api/nav_bar.php
@@ -0,0 +1,7 @@
+<a href="Index.html" accesskey="1">Home</a>
+<a href="Page2.html" accesskey="2">Page 2</a>
+<a href="Page3.html" accesskey="3">Page 3</a>
+<a href="Page4.html" accesskey="4">Page 4</a>
+<a href="Page5.html" accesskey="5">Page 5</a>
+<a href="Page6.html" accesskey="6">Page 6</a>
+<a href="Page7.html" accesskey="7">Page 7</a>
