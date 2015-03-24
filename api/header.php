0eb612cc40307bffa9fa7d728cc5d3d214f8952b
Fiona-Mary <fiona1harrison@gmail.com>
2015-03-24 13:21:29 +0000

    Update and rename header.php to api/header.php
    Moved to api, improved code, added search bar

---
+++ api/header.php
@@ -0,0 +1,8 @@
+
+    <h1>Company name</h1>
+    <p>Slogan..... Message.....</p>
+  <section id="SearchBar">
+  <form method="get" action="/search" id="search">
+    <input name="q" type="text" size="40" placeholder="Search..." />
+  </form>
+  </section>
